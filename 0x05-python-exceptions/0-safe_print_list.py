5609e12adb22308d8a284766289bf434872aef6e
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-10-23 15:09:09 -0700

    corrected an indentation error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[num]), end=" ")
             y += 1
     
-    except IndexError:
+        except IndexError:
             break
 
     print("")
