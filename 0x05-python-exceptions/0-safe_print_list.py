d25490d4885f073e80328076950399a385b89de2
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-10-23 15:07:05 -0700

    corrected the try position to be included within for loop range, used break instead of pass

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     y = x # y is num of elements
 
-    try:
-        for num in range(x):
+    for num in range(x):
+        try:
             print("{}".format(my_list[num]), end=" ")
             y += 1
     
     except IndexError:
-            pass
+            break
 
-    print()
+    print("")
     return x
