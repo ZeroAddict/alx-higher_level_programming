d5f23004c8a6b196d8e3ca22749d74d088f9127e
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-10-23 16:26:02 -0700

    changed he except in 0-safe_print_list and the for iter

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     y = 0 # y is num of elements
-    for num in range(x):
+    for i in range(x):
         try:
-            print("{}".format(my_list[num]), end=" ")
-            y += 1
+            print("{}".format(my_list[i]), end=" ")
+            y = y + 1
         except IndexError:
-            break
+            pass
     print("")
     return (y)
